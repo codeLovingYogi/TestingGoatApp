6d518d7682002579999c6ebc880e834e380e2006
Johnny <felichay@outlook.com>
2016-09-26 14:12:24 -0400

    Add first functional test and basic Django app

---
+++ functional_tests.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+# caps = DesiredCapabilities.FIREFOX
+# caps["marionette"] = True
+# caps["binary"] = "/usr/bin/firefox"
+#browser = webdriver.Firefox(capabilities=caps)
+browser = webdriver.PhantomJS()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
+
+browser.quit()
+ 
